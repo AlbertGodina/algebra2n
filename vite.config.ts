f291a1540d09d785b3e0e836be94c912fa2ca160
AlbertGodina <agodina1@xtec.cat>
2025-12-16 18:47:40 +0100

    Create vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/algebra2n/', // Important: ha de coincidir amb el nom del teu repositori
+});
